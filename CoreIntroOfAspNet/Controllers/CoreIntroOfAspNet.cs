40e2fc076561c79a0bf9259f2e20cee58b5094ee
Kirill1018 <115730059+Kirill1018@users.noreply.github.com>
2024-12-11 19:12:40 +0300

    Add files via upload

---
+++ CoreIntroOfAspNet/Controllers/CoreIntroOfAspNet.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreIntroOfAspNet.Controllers
+{
+    public class CoreIntroOfAspNet : Controller
+    {
+        string[] ToDo = { "task to do 1", "task to do 2", "task to do 3" };//массив дел для выполнения
+        public IActionResult Index()
+        {
+            ViewData["cases"] = this.ToDo;//объект библиотеки, в который добавляется информация, которая становится доступной для отображения на странице
+            return View();
+        }
+    }
+}
